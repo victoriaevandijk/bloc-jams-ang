8ad92c2ffda202ddcff9dd6585936cea413c60c9
torievandijk <victoriaevandijk@gmail.com>
2016-12-18 19:08:28 +0000

    Routing and States Assignment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,6 +14,10 @@
              url: '/album',
              templateUrl: '/templates/album.html'
          });
+         .state('collection', {
+             url: '/collection',
+             templateUrl: '/templates/collection.html'
+         });
      }
      
      angular
