4bc9b1ed5e14443b7e0c89bd80d35c86708784cd
torievandijk <victoriaevandijk@gmail.com>
2016-12-22 12:12:09 +0000

    Controllers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,13 +9,16 @@
          $stateProvider
          .state('landing', {
              url: '/',
+             controller: 'LandingCtrl as landing',
+             templateUrl: '/templates/landing.html'
          })
          .state('album', {
              url: '/album',
              templateUrl: '/templates/album.html'
-         });
+         })
          .state('collection', {
              url: '/collection',
+             controller: 'CollectionCtrl as collection',
              templateUrl: '/templates/collection.html'
          });
      }
