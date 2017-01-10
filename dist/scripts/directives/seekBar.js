a825bafec4519a9d7e048801cfe41b140d9fd5c8
torievandijk <victoriaevandijk@gmail.com>
2017-01-10 00:23:25 +0000

    Directives: Part 1- Assignment

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -29,6 +29,10 @@
                 scope.fillStyle = function() {
                     return {width: percentString()};
                 };
+                
+                scope.thumbStyle = function() {
+                    return {left: percentString()}; 
+                };
     
                 scope.onClickSeekBar = function(event) {
                      var percent = calculatePercent(seekBar, event);
